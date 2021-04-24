e1c289728e0361392968558da9e1fbccd80c4b7a
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-24 23:14:48 +0200

    Constant and Variable

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/ByungHaLee/learngo/something"
 )
 
 func main() {
-	fmt.Println("Hello World!")
-	// something.sayHello() // private
-	something.SayBye() // public
+	const name string = "constant"
+	var phone string = "variable"
+	fmt.Println(phone)
 }
