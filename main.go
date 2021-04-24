7741959ffc7be008edc08e73ab36ea8f645ac977
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-24 23:28:31 +0200

    multiple return values

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func multiply(a, b int) int {
-	return a * b
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
 }
 
 func main() {
-	fmt.Println(multiply(2, 3))
+	len, upper := lenAndUpper("Hello")
+	fmt.Println(len, upper)
 }
