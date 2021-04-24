1c015c623152f8eb1116d766c3ef4de1c4a40528
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-25 00:40:11 +0200

    If else

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import "fmt"
 
-func superAdd(numbers ...int) int {
-	total := 0
-	for _, number := range numbers {
-		total += number
+func canIDrink(age int) bool {
+	if age < 18 {
+		return false
+	} else {
+		return true
 	}
-	return total
 }
 func main() {
-	result := superAdd(1, 2, 3, 4, 5, 6)
-	fmt.Println(result)
+	fmt.Println(canIDrink(16))
 }
