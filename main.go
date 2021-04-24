5260dfaedd83bdd88300043775a4542668725e40
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-25 00:53:12 +0200

    slice

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	names := [5]string{"aaa", "bbbb", "ccccc", "dddddd"}
+	names := []string{"aaa", "bbbb", "ccccc", "dddddd"}
+	names = append(names, "eeeeeee")
 	fmt.Println(names)
 }
