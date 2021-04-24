b0ccdb3a9eb1ddfed9954e4c865e4bc7372b8651
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-25 00:48:03 +0200

    pointer

--- main.go
+++ main.go
@@ -2,18 +2,9 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	switch {
-	case age < 18:
-		return false
-	case age == 18:
-		return true
-	case age < 18:
-		return false
-	}
-
-	return false
-}
 func main() {
-	fmt.Println(canIDrink(16))
+	a := 2
+	b := &a
+	a = 10
+	fmt.Println(&a, b)
 }
