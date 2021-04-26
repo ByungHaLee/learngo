8d054b72ace4d7a16c3b2783529e03baeb00decf
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-26 11:06:42 +0200

    Clean

--- main.go
+++ main.go
@@ -1,44 +1,4 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"net/http"
-)
-
-var errRequestFailed = errors.New("request failed")
-
 func main() {
-	results := make(map[string]string)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-	}
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitUrl(url)
-		if err != nil {
-			result = "Failed"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
-}
-
-func hitUrl(url string) error {
-	fmt.Println("Checking:", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(url, err, resp.StatusCode)
-		return errRequestFailed
-	}
-	return nil
 }
