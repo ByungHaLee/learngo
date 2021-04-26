14faa2f59d97ffef1238e3bded45d1a853523830
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-26 15:18:38 +0200

    Delete file after provided

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"os"
 	"strings"
 
 	"github.com/ByungHaLee/learngo/scrapper"
 	"github.com/labstack/echo/v4"
 )
 
+const fileName string = "jobs.csv"
+
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
 	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
 	scrapper.Scrape(term)
-	return c.Attachment("jobs.csv", "jobs.csv")
+	return c.Attachment(fileName, fileName)
 }
 
 func main() {
