f86954676d78ddff2a1189e074f3acdf35ce98b5
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-26 10:10:00 +0200

    Clean

--- main.go
+++ main.go
@@ -1,13 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/ByungHaLee/learngo/accounts"
-)
-
 func main() {
-	account := accounts.NewAccount("me")
-	account.Deposit(10)
-	fmt.Println(account)
 }
