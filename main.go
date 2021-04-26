73abf51576600da861ea76eddb53e93e1af5502e
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-26 11:10:33 +0200

    without concurrent

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	niceCount("me")
+	niceCount("you")
+}
+
+func niceCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is nice", i)
+		time.Sleep(time.Second)
+	}
 }
