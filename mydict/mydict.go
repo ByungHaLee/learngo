3cd6d6b3704d3768a80debe1e37d8bacd46b8801
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-26 10:39:14 +0200

    Add add func

--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,6 +5,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("not found")
+var errWordExist = errors.New("the word already exist")
 
 func (d Dictionary) Search(word string) (string, error) {
 	value, exists := d[word]
@@ -12,4 +13,15 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+func (d Dictionary) Add(word string, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExist
+	}
+	return nil
 }
