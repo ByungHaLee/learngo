5c63caeac1de51bec1a701ce2918afe885e61d06
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-26 10:23:36 +0200

    Add dictionary as alias

--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/ByungHaLee/learngo/mydict"
+)
+
 func main() {
+	dictionary := mydict.Dictionary{}
+	dictionary["hello"] = "hello"
+	fmt.Println(dictionary)
 }
