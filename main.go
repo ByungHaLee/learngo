ef8e0c4f778730f239da7d5c20634f9147a2fae3
Byung Ha Lee <byungha.lee@metronom.com>
2021-04-26 13:04:05 +0200

    Fetch webpage and use goquery

--- main.go
+++ main.go
@@ -1,4 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var baseUrl string = "https://kr.indeed.com/jobs?q=python&limit=10"
+
 func main() {
+	getPages()
+}
+
+func getPages() int {
+	res, err := http.Get(baseUrl)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	fmt.Println(doc)
+
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)
+	}
 }
